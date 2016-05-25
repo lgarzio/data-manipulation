b3b9708ef912dde3cf8102b89a67b6b5b7073656
lgarzio <lori.m.garzio@gmail.com>
2016-05-25 15:50:49 -0400

    revised script, added filename as a column

--- appendCSV.py
+++ appendCSV.py
@@ -1,28 +1,31 @@
 #! /usr/local/bin/python
 
+import pandas as pd
 import os
 
 '''
 This script combines all ingest csvs cloned in a local repo from https://github.com/ooi-integration/ingestion-csvs
 '''
 
-def appendCSV(rootDir,output_file):
-    with open(output_file,'w') as outfile:
-        header = ''
-        for root,dirs,files in os.walk(rootDir):
-            for f in files:
-                if f.endswith('ingest.csv'):
-                    with open(os.path.join(root,f),'r') as csvfile:
-                        if header=='':
-                            header=csvfile.readline()
-                            outfile.write(header)
-                        else:
-                            csvfile.readline()
-                        for line in csvfile:
-                            outfile.write(line)
+rootdir = '/Users/lgarzio/Documents/repo/ooi-integration/ingestion-csvs'
 
-rootDir = '/Users/lgarzio/Documents/repo/ooi-integration/ingestion-csvs'
-output_file = '/Users/lgarzio/Documents/OOI/ingestion_csvs_concat.csv'
-#output_file = os.path.join(rootDir,'test.csv')
+df = pd.DataFrame()
+for root, dirs, files in os.walk(rootdir):
+    for f in files:
+        if f.endswith('ingest.csv'):
+            with open(os.path.join(root,f),'r') as csv_file:
+                filereader = pd.read_csv(csv_file)
 
-appendCSV(rootDir,output_file)
+                # add the file name as a column
+                filereader['ingest_csv_filename'] = str(f)
+
+                # split uframe_route to get driver name and add as a column
+                dr = filereader['uframe_route'].str.split('.').str[1]
+                dr = dr.str.replace('-', '_')
+                filereader['driver'] = dr
+
+                df = df.append(filereader)
+                #df = df.rename(columns={'Unnamed: 4':'ingest_csv_notes'})
+
+header = ['reference_designator','driver','uframe_route','data_source','filename_mask','ingest_csv_filename']
+df.to_csv('/Users/lgarzio/Documents/OOI/ingestion_csvs_concat.csv',index = False, columns = header, na_rep = 'NaN')
